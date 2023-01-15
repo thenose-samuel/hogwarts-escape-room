b1d54114ee0583f27c1d07303d14e494fd299ddf
thenose-samuel <skthaw1@gmail.com>
2023-01-15 15:58:56 +0530

    added remaining landing page components

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,8 @@
 import "../styles/LandingPage.css";
 import Navbar from "../components/Navbar";
 import Entrance from "../assets/images/entrance.png";
-
+import { ORGANISATION_NAME } from "../constants";
+import { ReactComponent as Social } from "../assets/images/Social.svg";
 function LandingPage() {
   return (
     <div className="landing">
@@ -31,6 +32,42 @@ function LandingPage() {
           <div>DON'T FORGET TO CARRY A PEN AND NOTEBOOK</div>
           <div style={{ paddingTop: "5%" }}>ESTIMATED TIME: XX-XX MINUTES</div>
         </div>
+      </div>
+      <div className="second">
+        <div className="second-container">
+          <div className="title">HOW TO PLAY</div>
+          <div className="text">
+            Use the <span style={{ fontStyle: "italic" }}>charm</span> to unlock
+            the <span style={{ fontWeight: "bold" }}>DOOR</span>, and leave the
+            muggle world to begin your first mission !
+          </div>
+          <div className="text2">
+            A{" "}
+            <a href="https://www.oprahdaily.com/entertainment/a32598018/harry-potter-spells-list/">
+              list of charms
+            </a>{" "}
+            to help you access places and objects no muggle could even dream of.
+          </div>
+        </div>
+        <div className="sized-box"></div>
+        <div className="second-container">
+          <div className="title">MORE ESCAPE ROOMS COMING SOON!</div>
+          <div className="text">
+            The Development team at {ORGANISATION_NAME} are constantly working
+            to deliver the best escape room experiences right to your digital
+            devices.
+          </div>
+        </div>
+        <div className="sized-box"></div>
+      </div>
+      <div className="footer">
+        <div className="title">
+          To Support us at Zodiac Labs, feel free to share this with your
+          colleagues and friends !
+        </div>
+        <Social className="socials"></Social>
+        <div className="end">created by the team @{ORGANISATION_NAME}</div>
+        <div className="sized-box-small"></div>
       </div>
     </div>
   );
