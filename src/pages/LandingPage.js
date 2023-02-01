01767c91eb47bf61c92ebe0b7f0ceb307c585062
Samuel Khongthaw <skthaw1@gmail.com>
2023-02-01 10:46:12 +0530

    ui fixes + removed warnings

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,33 +7,34 @@ import { ReactComponent as Social } from "../assets/images/Social.svg";
 function LandingPage() {
   return (
     <div className="landing">
-      <img src={BGImage} alt="bg-image" className="bg-image"></img>
+      <img src={BGImage} alt="bg" className="bg-image"></img>
       <div className="header">
         <Navbar />
         <div className="title">
           <div>Harry Potter</div>
-          <div style={{ fontSize: "30px" }}>Escape Room</div>
+          <div style={{ fontSize: "40px", paddingLeft: "15vh" }}>Escape Room</div>
         </div>
         <div className="info">
           Dark forces are at a rise, and you must use all your wits, exercise
           your precise knowledge of the Wizarding World to reach the safety of
           Hogwarts.
         </div>
-        <div className="how-to-container">
+        {/* <div className="how-to-container">
           <div className="how-to">How to Play</div>
           <div className="instructions">
             Use the <span style={{ fontStyle: "italic" }}>charm</span> to unlock
             the door, and leave the <br /> muggle world to begin your first
             mission !
           </div>
-        </div>
+        </div> */}
         <div className="entrance">
           <img src={Entrance} alt="entrance" />
         </div>
         <div className="lower-text">
-          <div>DON'T FORGET TO CARRY A PEN AND NOTEBOOK</div>
-          <div style={{ paddingTop: "5%" }}>ESTIMATED TIME: XX-XX MINUTES</div>
+          <div>DON'T FORGET TO CARRY A QUILL AND NOTEBOOK</div>
+          <div style={{ paddingTop: "5%", fontSize: "10px"}}>Estimated time: XX-XX MINUTES</div>
         </div>
+        <hr className="horizontal-line"></hr>
       </div>
       <div className="second">
         <div className="second-container">
